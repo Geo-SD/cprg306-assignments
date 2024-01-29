4e175315b999251f9ce70ad9fb413e2fe16dd861
George Dan <georgedan.20@gmail.com>
2024-01-29 10:09:26 -0700

    After changes in assignment 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+import Link from "next/link"
+export default function StudentInfo(){
+    return(
+        <>
+        <div>George Simon</div>
+        <Link href="https://github.com/Geo-SD?tab=repositories">this is my github </Link>
+        </>
+    )
+}
